6e3d730a6b15d942d60cac8a5038acd7cb472ac0
Atsushi Yamamoto <atsushi@yatsushi.com>
2026-02-15 16:34:47 -0800

    Add dependencies

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
-print("Hello world!")
+from one import get_greeting
+from two import get_source
+
+print(f"{get_greeting()} {get_source()}!")
---
+++ one.py
@@ -0,0 +1,2 @@
+def get_greeting():
+    return "hello world"
